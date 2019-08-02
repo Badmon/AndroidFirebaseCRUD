d816e3617170e84da786c51068335d12980c5c80
Badmon <jleonh@autonoma.edu.pe>
2019-08-02 16:32:47 -0500

    Commit

--- app/src/main/java/com/example/licoreriamateo/PostsListActivity.java
+++ app/src/main/java/com/example/licoreriamateo/PostsListActivity.java
@@ -6,15 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.ColorSpace;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 
 import com.example.licoreriamateo.models.Producto;
 import com.example.licoreriamateo.models.ViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.iid.FirebaseInstanceIdReceiver;
 
 public class PostsListActivity extends AppCompatActivity {
@@ -47,7 +52,28 @@ public class PostsListActivity extends AppCompatActivity {
 
 
     //Busqueda
-    private void
+    private void firebaseSearch(String searchText) {
+        Query firebaseSearchQuery = mRef.orderByChild("nombre").startAt(searchText).endAt(searchText + "\uf8ff");
+
+        FirebaseRecyclerAdapter<Producto, ViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Producto, ViewHolder>(
+                        Producto.class,
+                        R.layout.row,
+                        ViewHolder.class,
+                        firebaseSearchQuery
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Producto producto, int i) {
+
+                        viewHolder.setDetails(getApplicationContext(),producto.getNombre(),"Descripción: "+producto.getApellido(),"Precio: S/."+producto.getPassword(),producto.getCorreo());
+
+                    }
+                };
+
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+
 
     @Override
     protected void onStart() {
@@ -70,6 +96,13 @@ public class PostsListActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView
+        return super.onCreateOptionsMenu(menu);
+    }
 }
 
 
